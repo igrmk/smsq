731fd0779f87d89c5084d27ca6f890722757d43a
Igor Mikushkin <igor.mikushkin@gmail.com>
2020-04-05 06:13:05 +0400

    removing unused files

--- android/app/src/main/java/com/github/igrmk/smsq/helpers/Connectivity.kt
+++
@@ -1,9 +0,0 @@
-package com.github.igrmk.smsq.helpers
-
-import android.content.Context
-import android.net.ConnectivityManager
-
-fun Context.checkConnectivity(): Boolean {
-    val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    return cm.activeNetworkInfo?.isConnected == true
-}
